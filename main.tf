774031122ee8aac4f2ec3582c9458a63de2b65ab
Meghsbhavale <megha.bhavale@gmail.com>
2021-03-17 23:46:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 provider "aws" {
-    region = "us-east-1"
+        region = "us-east-1"
 }
 
-resource "aws_instance" "ec2First" {
-    ami = "<Enter ami id here>"
-    instance_type = "<enter instance type required here>"
-    subnet_id          = "<Enter subnet id here>"
-    vpc_security_group_ids = ["<Entere security group id here>"]
-    tags               =  { 
-  
-         Name = "TerrEc2"
-                        }
-    
-}%           
+
+resource "aws_instance" "my-instance" {
+        ami = "ami-0be2609ba883822ec"
+        instance_type = "t2.micro"
+    subnet_id          = "<subnet -id>"
+    vpc_security_group_ids = ["<put security group id>"]
+        user_data = file("install_docker.sh")
+        tags = {
+                Name = "Terraform"
+                Value = "Test"
+        }
+}%                           
