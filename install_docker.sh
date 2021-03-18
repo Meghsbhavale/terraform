afcc6a2edbc285686349a8b30020cac315f32a83
Meghsbhavale <megha.bhavale@gmail.com>
2021-03-17 22:59:07 -0400

    Naming as test ,so when you do docker ps -a | grep test .You should see the container running

--- install_docker.sh
+++ install_docker.sh
@@ -6,13 +6,4 @@ sudo usermod -a -G docker ec2-user
 docker info
 echo "Installed Docker"
 sudo docker pull nginx
-docker build -t momo89/go-hello-world .
-docker push momo89/go-hello-world:latest
-docker run -p 3000:3000 -d momo89/go-hello-world
-cd $HOME
-mkdir nginx
-cd nginx
-cat nginx.conf
-cat Dockerfile
-docker build -t momo89/nginx .
-docker run -p 80:80 -d momo89/nginx
+docker run --name test -it nginx
